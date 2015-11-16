fbd263c6f05baf41f600fee6fa89aba8c499ca4c
Matej <matejdro@gmail.com>
2015-11-16 17:38:15 +0100

    Add new tertiary text (raw)

--- src/NotificationsWindow/tertiary_text.c
+++ src/NotificationsWindow/tertiary_text.c
@@ -1,50 +1,46 @@
-#include <pebble.h>
-#include "ActionsMenu.h"
-#include "../NotificationCenter.h"
-#include "Comm.h"
+/**
+ * Tertiary Text, by  vgmoose and PeterSumm
+ * https://github.com/PeterSumm/tertiary_text
+ */
+
+#include "tertiary_text.h"
+
+// Max text limit in characters
+// You may adjust this to allow longer messages
+#define MAX_CHARS 60
 
 #define TOP 0
 #define MID 1
 #define BOT 2
 
-#define NOTEPAD_TEXT 1
-#define NOTEPAD_CHAR_COUNT 2
+static Window* window;
 
-#define TEXT_LIMIT 60
-
-static Window* window = NULL;
-
-//static TextLayer* text_layer;
-static TextLayer* wordsYouWrite;
+static TextLayer* text_title;
+// static TextLayer* text_layer;
+static TextLayer* text_input;
 
 static TextLayer* buttons1[3];
 static TextLayer* buttons2[3];
 static TextLayer* buttons3[3];
 static TextLayer** bbuttons[3];
 
-static Layer* actionsProxyLayer;
-
-#ifdef PBL_SDK_3
-    static StatusBarLayer* statusBar;
-#endif
-
 static bool menu = false;
 
 // Here are the three cases, or sets
-static char caps[] =    "ABCDEFGHIJKLM NOPQRSTUVWXYZ";
-static char letters[] = "abcdefghijklm nopqrstuvwxyz";
-static char numsym[] = "1234567890!?-'\"$()&*+#:@/,.";
+static char caps[] =    	"ABCDEFGHIJKLM NOPQRSTUVWXYZ";
+static char letters[] = 	"abcdefghijklm nopqrstuvwxyz";
+static char numsym[] = 		"1234567890!?-'\"$()&*+#:@/,.";
 
 // the below three strings just have to be unique, abc - xyz will be overwritten with the long strings above
-static char* btext1[] = {"abc\0", "def\0", "ghi\0"};
-static char* btext2[] = {"jkl\0", "m n\0", "opq\0"};
-static char* btext3[] = {"rst\0", "uvw\0", "xyz\0"};
+static char* btext1[] = {"abc", "def", "ghi"};
+static char* btext2[] = {"jkl", "m n", "opq"};
+static char* btext3[] = {"rst", "uvw", "xyz"};
 static char** btexts[] = {btext1, btext2, btext3};
 
 // These are the actual sets that are displayed on each button, also need to be unique
-static char set1[4] = "  a\0";
-static char set2[4] = "  b\0";
-static char set3[4] = "  c\0";
+static char set1[4] = "  a";
+static char set2[4] = "  b";
+static char set3[4] = "  c";
 static char* setlist[] = {set1, set2, set3};
 
 static char* cases[] = {"CAP", "low", "#@1"};
@@ -61,10 +57,15 @@ static void next();
 
 static char* master = letters;
 
-
-static char text_buffer[TEXT_LIMIT + 1];
+static char text_buffer[MAX_CHARS];
 static int pos = 0;
 static int top, end, size;
+
+static const char* title;
+static void* extra;
+static TertiaryTextCallback callback;
+
+static const bool animated = true;
 
 // This function changes the next case/symbol set.
 static void change_set(int s, bool lock)
@@ -82,10 +83,10 @@ static void change_set(int s, bool lock)
             }
         }
     }
-    
+
     menu = false;
     if (lock) cur_set = s;
-    
+
     drawSides();
 }
 
@@ -98,113 +99,54 @@ static void next()
 
 static void clickButton(int b)
 {
-	if (menu)
-	{
-		change_set(b, false);
-		return;
-	}
-
-	if (size > 3)
-	{
-		size /= 3;
-		if (b == TOP)
-		end -= 2*size;
-		else if (b == MID)
-		{
-			top += size;
-			end -= size;
-		}
-		else if (b == BOT)
-		top += 2*size;
-	}
-	else
-	{
-		if (pos < TEXT_LIMIT)
-		{
-			text_buffer[pos++] = master[top+b];
-			drawNotepadText();
-		}
-
-		change_set(cur_set, false);
-		next();
-	}
-
-	drawSides();
-    
-}
-
-// Modify these common button handlers
-static void up_single_click_handler(ClickRecognizerRef recognizer, void* context) {
-    if (actions_menu_is_displayed())
+    if (menu)
     {
-		actions_menu_move_up();
-    	return;
+        change_set(b, false);
+        return;
     }
 
+    if (size > 3)
+    {
+        size /= 3;
+        if (b == TOP)
+            end -= 2*size;
+        else if (b == MID)
+        {
+            top += size;
+            end -= size;
+        }
+        else if (b == BOT)
+            top += 2*size;
+    }
+    else
+    {
+        // Prevent overflowing
+        if (pos == MAX_CHARS-1)
+            pos--;
+
+        // Type the character
+        text_buffer[pos++] = master[top+b];
+        drawNotepadText();
+        change_set(cur_set, false);
+        next();
+    }
+
+    drawSides();
+}
+
+static void up_single_click_handler(ClickRecognizerRef recognizer, void* context)
+{
     clickButton(TOP);
-    
 }
 
-static void up_repeating_click_handler(ClickRecognizerRef recognizer, void* context) {
-
-	if (actions_menu_is_displayed())
-		actions_menu_move_up();
-}
-
-static void select_single_click_handler(ClickRecognizerRef recognizer, void* context) {
-	if (actions_menu_is_displayed())
-	{
-		int16_t pickedItem = actions_menu_get_selected_index();
-		if (pickedItem == 0)
-		{
-            nw_send_reply_text(text_buffer);
-			actions_menu_hide();
-			window_stack_pop(true);
-		}
-		else
-		{
-			char* copySource = actions_menu_get_action_text(pickedItem);
-			int sourceLength = strlen(copySource);
-
-			if (sourceLength + pos < TEXT_LIMIT)
-			{
-				strcpy(&text_buffer[pos], copySource);
-				pos += sourceLength;
-				drawNotepadText();
-				actions_menu_hide();
-			}
-		}
-
-		return;
-	}
-
+static void select_single_click_handler(ClickRecognizerRef recognizer, void* context)
+{
     clickButton(MID);
 }
 
-static void down_single_click_handler(ClickRecognizerRef recognizer, void* context) {
-    
-    if (actions_menu_is_displayed())
-    {
-		actions_menu_move_down();
-    	return;
-    }
-
+static void down_single_click_handler(ClickRecognizerRef recognizer, void* context)
+{
     clickButton(BOT);
-}
-
-static void down_repeating_click_handler(ClickRecognizerRef recognizer, void* context) {
-
-	if (actions_menu_is_displayed())
-		actions_menu_move_down();
-}
-
-
-static void back_single_click_handler(ClickRecognizerRef recognizer, void* context) {
-
-	if (actions_menu_is_displayed())
-		actions_menu_hide();
-	else
-		window_stack_pop(true);
 }
 
 static bool common_long(int b)
@@ -217,33 +159,34 @@ static bool common_long(int b)
     return false;
 }
 
-static void up_long_click_handler(ClickRecognizerRef recognizer, void* context) {
-    
+static void up_long_click_handler(ClickRecognizerRef recognizer, void* context)
+{
     if (common_long(TOP)) return;
-    
+
     set_menu();
-    
 }
 
-static void select_long_click_handler(ClickRecognizerRef recognizer, void* context) {
-    
+static void select_long_click_handler(ClickRecognizerRef recognizer, void* context)
+{
     if (common_long(MID)) return;
 
-    actions_menu_set_number_of_items(10);
-    actions_menu_show();
+    // Close this window
+    window_stack_pop( animated );
+
+    // Call user supplied callback with text_buffer
+    callback( text_buffer, strlen( text_buffer ), extra );
 
 }
 
 
 static void down_long_click_handler(ClickRecognizerRef recognizer, void* context) {
-    
+
     if (common_long(BOT)) return;
-    
+
     // delete or cancel when back is held
-    
-    if (size==27 && pos>0)
+    if (size==27 && pos>0 )
     {
-        text_buffer[--pos] = 0;
+        text_buffer[--pos] = ' ';
         drawNotepadText();
     }
     else
@@ -251,32 +194,25 @@ static void down_long_click_handler(ClickRecognizerRef recognizer, void* context
         next();
         drawSides();
     }
-    
+
 }
 
 static void set_menu()
 {
-	menu = true;
-	drawMenu();
+    menu = true;
+    drawMenu();
 }
 
-// This usually won't need to be modified
-
-static void click_config_provider(void* context) {
-    
-	window_single_click_subscribe(BUTTON_ID_UP, up_single_click_handler);
-    window_long_click_subscribe(BUTTON_ID_UP, 500, up_long_click_handler, NULL);
-	//window_single_repeating_click_subscribe(BUTTON_ID_UP, 50, up_repeating_click_handler);
+static void click_config_provider(void* context)
+{
+    window_single_click_subscribe(BUTTON_ID_UP, up_single_click_handler);
+    window_long_click_subscribe(BUTTON_ID_UP, 1000, up_long_click_handler, NULL);
 
     window_single_click_subscribe(BUTTON_ID_SELECT, select_single_click_handler);
-    window_long_click_subscribe(BUTTON_ID_SELECT, 500, select_long_click_handler, NULL);
-    
+    window_long_click_subscribe(BUTTON_ID_SELECT, 1000, select_long_click_handler, NULL);
+
     window_single_click_subscribe(BUTTON_ID_DOWN, down_single_click_handler);
-    window_long_click_subscribe(BUTTON_ID_DOWN, 500, down_long_click_handler, NULL);
-	//window_single_repeating_click_subscribe(BUTTON_ID_DOWN, 50, down_repeating_click_handler);
-
-    window_single_click_subscribe(BUTTON_ID_BACK, back_single_click_handler);
-
+    window_long_click_subscribe(BUTTON_ID_DOWN, 1000, down_long_click_handler, NULL);
 }
 
 static void drawMenu()
@@ -285,7 +221,7 @@ static void drawMenu()
     {
         text_layer_set_text(bbuttons[i][i!=2], " ");
         text_layer_set_text(bbuttons[i][2], " ");
-        
+
         text_layer_set_text(bbuttons[i][i==2], cases[i]);
         text_layer_set_font(bbuttons[i][0], fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
     }
@@ -306,135 +242,150 @@ static void drawSides()
                 text_layer_set_background_color(bbuttons[h][i], GColorClear);
                 text_layer_set_font(bbuttons[h][i], fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
             }
-            
+
         }
     }
     else if (size==9)   // second click
     {
-        
         for (int i=0; i<3; i++)
         {
             text_layer_set_text(bbuttons[i][i!=2], " ");
             text_layer_set_text(bbuttons[i][2], " ");
-            
+
             text_layer_set_text(bbuttons[i][i==2], btexts[top/9][i]);
             text_layer_set_font(bbuttons[i][i==2], fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
         }
-        
-    } else if (size == 3)
+
+    }
+    else if (size == 3)
     {
         for (int i=0; i<3; i++)
         {
             setlist[i][2] = master[top+i];
             text_layer_set_text(bbuttons[i][i==2], setlist[i]);
-            
+            text_layer_set_font(bbuttons[i][i==2], fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
         }
     }
-    
+}
+
+static void clearText()
+{
+    for (int i=0; i<MAX_CHARS; i++)
+        text_buffer[i] = '\0';
+    pos = 0;
 }
 
 static void initSidesAndText()
 {
+    // Make sure the text is cleared on the first launch
+    clearText();
+
+    // Retrieve the window layer and its bounds
     Layer *window_layer = window_get_root_layer(window);
+// 		GRect bounds = layer_get_bounds(window_layer);
 
-    wordsYouWrite = text_layer_create((GRect) { .origin = { 10, STATUSBAR_Y_OFFSET }, .size = { 100, 150 } });
+    // Create a text layer for the text that is typed - WTF? - it was never used!
+//     text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
+//     text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
+//     layer_add_child(window_layer, text_layer_get_layer(text_layer));
 
-    text_layer_set_background_color(wordsYouWrite, GColorClear);
-    text_layer_set_font(wordsYouWrite, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
-    layer_add_child(window_layer, text_layer_get_layer(wordsYouWrite));
-    
+    // Create a text layer for the title
+#if defined(PBL_ROUND)
+    text_title = text_layer_create( GRect( 34, 16, 86, 32 ) );
+#else
+    text_title = text_layer_create( GRect( 3, 0, 107, 32 ) );
+#endif
+    text_layer_set_font( text_title, fonts_get_system_font( FONT_KEY_GOTHIC_14_BOLD ) );
+    text_layer_set_text( text_title, title );
+    text_layer_set_text( text_title, "This is a test and another test." );
+    layer_add_child( window_layer, text_layer_get_layer( text_title ) );
+
+    // Create a text layer for the text that has been typed - PRS
+#if defined(PBL_ROUND)
+    text_input = text_layer_create((GRect) { .origin = { 32, 40 }, .size = { 90, 120 } });
+    text_layer_set_font(text_input, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+#else
+    text_input = text_layer_create((GRect) { .origin = { 3, 24 }, .size = { 110, 150 } });
+    text_layer_set_font(text_input, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+#endif
+
+    text_layer_set_background_color(text_input, GColorClear);
+    layer_add_child(window_layer, text_layer_get_layer(text_input));
+
     for (int i = 0; i<3; i++)
     {
-        buttons1[i] = text_layer_create((GRect) { .origin = { 115, 12*i + STATUSBAR_Y_OFFSET }, .size = { 100, 100 } });
-        buttons2[i] = text_layer_create((GRect) { .origin = { 115, 12*i+50 + STATUSBAR_Y_OFFSET }, .size = { 100, 100 } });
-        buttons3[i] = text_layer_create((GRect) { .origin = { 115, 12*i+100 + STATUSBAR_Y_OFFSET }, .size = { 100, 100 } });
+#if defined(PBL_ROUND)
+        buttons1[i] = text_layer_create((GRect) { .origin = { 125, 12*i+14 }, .size = { 100, 100 } });
+        buttons2[i] = text_layer_create((GRect) { .origin = { 125, 12*i+64 }, .size = { 100, 100 } });
+        buttons3[i] = text_layer_create((GRect) { .origin = { 125, 12*i+114 }, .size = { 100, 100 } });
+#else
+        buttons1[i] = text_layer_create((GRect) { .origin = { 115, 12*i }, .size = { 100, 100 } });
+        buttons2[i] = text_layer_create((GRect) { .origin = { 115, 12*i+50 }, .size = { 100, 100 } });
+        buttons3[i] = text_layer_create((GRect) { .origin = { 115, 12*i+100 }, .size = { 100, 100 } });
+#endif
     }
-    
-    for (int i=0; i<3; i++)
-        for (int j=0; j<3; j++)
-            layer_add_child(window_layer, text_layer_get_layer(bbuttons[i][j]));
-    
-    actionsProxyLayer = layer_create(GRect(0, -16 + STATUSBAR_Y_OFFSET, 144, 168));
-    actions_menu_attach(actionsProxyLayer);
-    layer_add_child(window_layer, actionsProxyLayer);
+
+    for( int i=0; i<3; i++ )
+        for( int j=0; j<3; j++ )
+            layer_add_child( window_layer, text_layer_get_layer( bbuttons[i][j] ) );
+
 }
 
 static void drawNotepadText()
 {
-    text_layer_set_text(wordsYouWrite, text_buffer);
+    text_layer_set_text(text_input, text_buffer);
 }
 
-static void deinit(void) {
-    text_buffer[pos] = '\0';
-    
-    text_layer_destroy(wordsYouWrite);
-    for (int i = 0; i<3; i++)
-	{
-        text_layer_destroy(buttons1[i]);
-        text_layer_destroy(buttons2[i]);
-        text_layer_destroy(buttons3[i]);
-	}
+static void window_unload(Window *window)
+{
+//   text_layer_destroy(text_layer);
+    text_layer_destroy(text_input);
+    text_layer_destroy(text_title);
 
-#ifdef PBL_SDK_3
-    status_bar_layer_destroy(statusBar);
-#endif
+    for( int i=0; i<3; i++ )
+        for( int j=0; j<3; j++ )
+            text_layer_destroy( bbuttons[ i ][ j ] );
 
     window_destroy(window);
 }
 
-static void window_unload(Window *window) {
-    deinit();
-}
-
 static void window_load(Window* window)
 {
-    Layer *topLayer = window_get_root_layer(window);
-
-#ifdef PBL_SDK_3
-    statusBar = status_bar_layer_create();
-    layer_add_child(topLayer, status_bar_layer_get_layer(statusBar));
-#endif
-
     initSidesAndText();
     drawSides();
-
+    drawNotepadText();
 }
 
-static void init(void) {
-	//List window needs to close, otherwise Pebble runs out of RAM.
-	//close_notification_list_window();
-
-    window = window_create();
-    
-    text_buffer[0] = 0;
-    pos = 0;
-    
-    window_set_click_config_provider(window, click_config_provider);
-    window_set_window_handlers(window, (WindowHandlers) {
-        .load = window_load,
-        .unload = window_unload,
-    });
-    
-    const bool animated = true;
-    window_stack_push(window, animated);
-
-}
-
-void tertiary_text_window_close()
+void tertiary_text_prompt( const char* _title, TertiaryTextCallback _callback, void* _extra )
 {
-	if (window != NULL)
-		window_stack_remove(window, true);
-}
+    // Store the arguments for later
+    title = _title;
+    extra = _extra;
+    callback = _callback;
 
-void tertiary_text_window_init() {
+    // Setup the button arrays
     bbuttons[0] = buttons1;
     bbuttons[1] = buttons2;
     bbuttons[2] = buttons3;
-    init();
+
+    // Create and configure the window
+    window = window_create();
+#ifndef PBL_SDK_3
+    window_set_fullscreen(window,true);
+#endif
+    window_set_click_config_provider(window, click_config_provider);
+
+    window_set_window_handlers(window, (WindowHandlers) {
+            .load = window_load,
+            .unload = window_unload,
+    });
+
+    // Default to lowercase letters
     change_set(1, true);
+
+    // Initiate the character position and update the text layers
     next();
-    drawSides();
-    drawNotepadText();
-    
-    //deinit();
+
+    // Push the window onto the stack
+    window_stack_push(window, animated);
 }
--- src/NotificationsWindow/tertiary_text.h
+++ src/NotificationsWindow/tertiary_text.h
@@ -1,7 +1,19 @@
-#ifndef TERTIARYTEXT_H_
-#define TERTIARYTEXT_H_
+#include <pebble.h>
 
-void tertiary_text_window_init();
-void tertiary_text_window_close();
+// A callback which is called when a user submits data
+//
+// Params:
+// result - The data the user typed. Memory is shared across calls to tertiary_text_prompt, so copy it before calling again
+// result_lengh - Number of characters in result
+// extra - The extra data passed to tertiary_text_prompt
+//
+typedef void(* TertiaryTextCallback)( const char* result, size_t result_length, void* extra );
 
-#endif
+// Opens a tertiary text window and gets user input
+//
+// Params:
+// title - Some text to display on the top of the input screen
+// callback - The function to call when input is complete
+// extra - Some extra data to pass to the callback
+//
+void tertiary_text_prompt( const char* title, TertiaryTextCallback callback, void* extra );
