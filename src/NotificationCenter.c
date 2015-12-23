2a5d39a41d4f706a61a3e9153e33c02e6c30363c
Matej <matejdro@gmail.com>
2015-12-23 09:37:57 +0100

    Protocol bump

--- src/NotificationCenter.c
+++ src/NotificationCenter.c
@@ -5,7 +5,7 @@
 #include "NotificationListWindow.h"
 #include "NotificationsWindow/Comm.h"
 
-const uint16_t PROTOCOL_VERSION = 33;
+const uint16_t PROTOCOL_VERSION = 34;
 
 int8_t curWindow = 0;
 bool gotConfig = false;
